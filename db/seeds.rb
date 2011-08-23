06ea47917ff0c438ce07dd37ac9bb6c7fb5bd5dc
MadPittsPilot <martin@i-byte.com.au>
2011-08-23 18:31:23 +1000

    Seed data. Country and Locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+require 'open-uri'
+#require 'active_record/fixtures'
+
+#["Windows", "Linux", "Mac OS X"].each do |os|
+#  OperatingSystem.find_or_create_by_name(os)
+#end
+
+Country.delete_all
+open("http://openconcept.ca/sites/openconcept.ca/files/country_code_drupal_0.txt") do |countries|
+  countries.read.each_line do |country|
+    code, name = country.chomp.split("|")
+    Country.create!(:country_long => name, :country => code)
+#    Country.create!(:name => name, :code => code)
+  end
+end
+
+["Home", "Work", "Mobile"].each do |loc|
+  Location.find_or_create_by_location(loc)
+end
+
+
+#Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "operating_systems")
