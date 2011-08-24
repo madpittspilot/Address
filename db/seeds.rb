cd48fa7ffac4981cb38710660e69daa98cc4bd16
madpittspilot <martin@i-byte.com.au>
2011-08-24 16:39:28 +1000

    more seed.db stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,21 @@
 require 'open-uri'
 #require 'active_record/fixtures'
 
-#["Windows", "Linux", "Mac OS X"].each do |os|
-#  OperatingSystem.find_or_create_by_name(os)
+
+#Country.delete_all
+#open("http://openconcept.ca/sites/openconcept.ca/files/country_code_drupal_0.txt") do |countries|
+#  countries.read.each_line do |country|
+#    code, name = country.chomp.split("|")
+#    Country.create!(:country_long => name, :country => code)
+##    Country.create!(:name => name, :code => code)
+#  end
 #end
 
-Country.delete_all
+#Country.delete_all
 open("http://openconcept.ca/sites/openconcept.ca/files/country_code_drupal_0.txt") do |countries|
   countries.read.each_line do |country|
     code, name = country.chomp.split("|")
-    Country.create!(:country_long => name, :country => code)
+    Country.find_or_create_by_country(:country_long => name, :country => code)
 #    Country.create!(:name => name, :code => code)
   end
 end
@@ -25,6 +31,15 @@ end
 ["Home", "Work", "Mobile"].each do |loc|
   Location.find_or_create_by_location(loc)
 end
+
+
+State.find_or_create_by_state(:state => "ACT", :state_long => "Asses Culprits and Tricksters", :country_id => 13 )
+State.find_or_create_by_state(:state => "NSW", :state_long => "New South Wales", :country_id => 13 )
+State.find_or_create_by_state(:state => "NT", :state_long => "Northern Territory", :country_id => 13 )
+State.find_or_create_by_state(:state => "QLD", :state_long => "Queensland", :country_id => 13 )
+State.find_or_create_by_state(:state => "SA", :state_long => "South Australia", :country_id => 13 )
+State.find_or_create_by_state(:state => "TAS", :state_long => "Tasmania", :country_id => 13 )
+State.find_or_create_by_state(:state => "WA", :state_long => "Western Australia", :country_id => 13 )
 
 
 #Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "operating_systems")
